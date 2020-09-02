bc7ac221b46333a44f2bf980492d3a4b174abd6e
saleh-jaz1@hotmail.com <P@$$w0rd>
2020-09-02 03:01:17 +0300

    Apigee Oauth2 Authorization Code Demo

---
+++ config/config.js
@@ -0,0 +1,22 @@
+
+'use strict';
+
+/*
+## Cloud users: Leave the default value for $api_domain.
+## On-prem customers:
+## - Change the $url to your Apigee management server.
+## - Change $domain to the base domain for your own Apigee API calls.
+*/
+
+// exports.envInfo = {
+//   org: 'ORGNAME',
+//   env: 'ENVNAME',
+//   domain: 'apigee.net'
+// };
+
+exports.envInfo ={
+  url: 'saleh-jaz1-09360-eval-prod.apigee.net',
+  basicToken: 'Basic QVJUSFFDczNPU3h6RkxlQTBHRkExMkVYcHRLaVhBWlk6OEk0bk01VnYzZmQyeVozcw==',
+  host: 'localhost',
+  port: '3000'
+}
